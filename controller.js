e227f66cf3e2760326b3383aa38b826b18f6af90
Vivek G.S <vivekgs2007@gmail.com>
2014-03-17 23:10:52 +0530

    removed files from git

--- controller.js
+++
@@ -1,9 +0,0 @@
-app.controller('d3donut',['$scope','jsondata',function($scope,jsondata){
-	$scope.jsonObjs = {};
-	$scope.pushJson = function(){
-		jsondata.getJsonData().then(function(response){
-        	$scope.jsonObjs = response.data;
-    	});
-		
-	};
-}]);
