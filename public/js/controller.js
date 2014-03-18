749c2987fdf95b15c23834d91672750cff6241ed
Vivek G.S <vivekgs2007@gmail.com>
2014-03-18 19:04:49 +0530

    Final Updated code

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,5 +1,6 @@
 app.controller('d3donut',['$scope','jsondata',function($scope,jsondata){
-
+	$scope.jsonObjs = [];
+	
 	jsondata.getJsonData().then(function(response){			
         	$scope.jsonObjs = response.data;
     	});
@@ -11,5 +12,8 @@ app.controller('d3donut',['$scope','jsondata',function($scope,jsondata){
 			formObj.d3 = $scope.d3;
 			$scope.jsonObjs.push(formObj);
 		}				
+	};
+	$scope.removeJson = function(jsonIndex){
+		$scope.jsonObjs.splice(jsonIndex, 1);
 	};
 }]);
--- public/js/directive.js
+++ public/js/directive.js
@@ -1,44 +1,53 @@
 app.directive('d3chart', function (jsondata) {
-  jsondata.getJsonData().then(function(response){   
     return {
-      restrict: 'EA',
-      scope: {
-          dataArr: "="
-        } ,
-      link: function (scope, elem, attrs) {
-        var rows = response.data;
-        alert(elem);
-        var dataArr = [];
-        for(row in rows){
-            dataArr.push(rows[row].d3);
-        }
-        /*var width = 460,
-            height = 300,
+      restrict: 'E',      
+      link: function (scope, elem, attrs) {        
+        jsondata.getJsonData().then(function(response){   
+
+          var rows = response.data;
+          var dataArr = [];
+          for(row in rows){
+              dataArr.push(rows[row].d3);
+              // dataArr.push(rows[row].angular); // TO view the angular
+          }
+          var width = 560,
+            height = 600,
             radius = Math.min(width, height) / 2;
 
-        var color = d3.scale.category20();
+          var svg = d3.select(elem[0])
+              .append("svg")
+              .attr("width", width)
+              .attr("height", height)
+              .append("g")
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-        var pie = d3.layout.pie()
-            .sort(null);
+          scope.$watchCollection("jsonObjs", function() {
+              var dataArr = [];
+              for(row in scope.jsonObjs){
+                  dataArr.push(scope.jsonObjs[row].d3);
+              }              
+              scope.rerender(dataArr);
+          });
 
-        var arc = d3.svg.arc()
-            .innerRadius(radius - 100)
-            .outerRadius(radius - 50);
+          scope.rerender = function(dataArr){ 
+            svg.selectAll("*").remove();           
+            var color = d3.scale.category20();
 
-        var svg = d3.select(elem[0]).append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+            var pie = d3.layout.pie()
+                .sort(null);
 
-        var path = svg.selectAll("path")
-            .data(pie(dataArr))
-          .enter().append("path")
-            .attr("fill", function(d, i) { return color(i); })
-            .attr("d", arc);*/
+            var arc = d3.svg.arc()
+                .innerRadius(radius - 100)
+                .outerRadius(radius - 50);
 
+            var path = svg.selectAll("path")
+                .data(pie(dataArr))
+              .enter().append("path")
+                .attr("fill", function(d, i) { return color(i); })
+                .attr("d", arc);
+          }  
+          scope.rerender(dataArr);
+        });
       }
-    }
-  });
-    
+    }    
 });    
